5d9a1e6e10ab05bed9de8b619df8b916ff0828d9
djc <1002637814@qq.com>
2022-09-01 20:34:04 +0800

    Initial commit

---
+++ src/test/java/com/djc/MyLeetCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLeetCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
