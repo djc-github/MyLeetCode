0d64672a3891240c577a62e9203c195a013832b9
djc <1002637814@qq.com>
2022-11-03 16:59:46 +0800

    hanhan

--- src/test/java/com/djc/MyLeetCodeApplicationTests.java
+++ src/test/java/com/djc/MyLeetCodeApplicationTests.java
@@ -3,11 +3,28 @@ package com.djc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.util.Date;
+
 @SpringBootTest
 class MyLeetCodeApplicationTests {
 
     @Test
     void contextLoads() {
+        long currentTimeMillis = System.currentTimeMillis();
+        LocalDateTime now1 = LocalDateTime.now();
+        Instant instant = Instant.now();
+        long epochSecond = instant.getEpochSecond();
+        long toEpochMilli = instant.toEpochMilli();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        LocalDateTime localDateTime2 = LocalDateTime.ofEpochSecond(epochSecond, instant.getNano(),ZoneOffset.ofHours(+8));
+        System.out.println(localDateTime);
+        System.out.println(localDateTime2);
+        System.out.println(now1.toString().replace("T"," "));
+        LocalDateTime localDateTime1 = LocalDateTime.of(2022, Month.SEPTEMBER, 3, 18, 10, 10,1);
+        System.out.println(localDateTime1);
+
     }
 
 }
